a92a5a5e5347f108522275a1cab38da037f07ab5
Mo Amin <m.alamin92@gmail.com>
2015-12-10 18:38:17 -0500

    window routes

--- app/controllers/windows_controller.rb
+++ app/controllers/windows_controller.rb
@@ -1,5 +1,31 @@
 class WindowsController < ApplicationController
+	def new
+		@window = Window.new	
+	end
+
+	def show
+		@window = Window.find(params[:id])
+	end
+
 	def index
 		@windows = Window.all
+	end
+
+	def create
+
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def window_params
+		params.rquire(:window).permit(:)
 	end
 end
