e152980053d79b5179ee2d349ad15c01ef14ced2
Mo Amin <m.alamin92@gmail.com>
2015-12-13 01:03:17 -0500

    migrate image for window

--- app/models/window.rb
+++ app/models/window.rb
@@ -1,4 +1,7 @@
 class Window < ActiveRecord::Base
 	validates :store_name, presence: true, length: {maximum: 80}
 	validates :location, presence: true, length: {maximum: 80}
+
+	has_attached_file :image, styles: { large: "600x600", medium: "300x300#", thumb: "100x100>" }
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
