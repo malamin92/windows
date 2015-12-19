2684480a11a189f9cf04a3025abcf6194620c4ef
Mo Amin <m.alamin92@gmail.com>
2015-12-19 00:50:02 -0500

    add user and window association

--- app/models/window.rb
+++ app/models/window.rb
@@ -7,6 +7,8 @@ class Window < ActiveRecord::Base
 
 	has_attached_file :image, styles: { large: "600x600", medium: "300x300#", thumb: "100x100>" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	
+	belongs_to :user
 
   	def titleize_name
   		self.store_name = self.store_name.titleize
