4ece51e80886d2ec6bafc721a363aefcbe4631cd
Mo Amin <m.alamin92@gmail.com>
2015-12-28 16:29:35 -0500

    home exceptions fix

--- app/controllers/windows_controller.rb
+++ app/controllers/windows_controller.rb
@@ -1,5 +1,5 @@
 class WindowsController < ApplicationController
-	before_action :authenticate_user!, except: [:index, :show]
+	before_action :authenticate_user!, except: [:index, :show, :home]
 	
 	def new
 		@window = current_user.windows.build	
