41d166d540bb9e5c4a568fa26f8987ef4a49e975
Mo Amin <m.alamin92@gmail.com>
2015-12-28 06:27:56 -0500

    deletable comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :windows
+  resources :windows do 
+    resources :comments, only: [:create, :destroy]
+  end
 
   # You can have the root of your site routed with "root"
   root 'windows#home'
 
   get 'my_windows' => 'windows#user_windows'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
